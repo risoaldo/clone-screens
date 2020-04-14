84a74eb005864e7378a7066fa70105c6cccd9279
Risoaldo <noobregarisoaldo@gmail.com>
2020-04-13 21:47:12 -0300

    new screen drawer

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -5,36 +5,65 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        width: MediaQuery.of(context)
+            .size
+            .width, // imprime o tamanho da largura da tela
+        height: MediaQuery.of(context)
+            .size
+            .height, // imprime o tamanho da altura da tela
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [Color(0Xff00CCFF), Color(0Xff009AFF)],
+          ),
+        ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: <Widget>[
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: 300,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
-                  colors: [Color(0XffF3831F), Color(0XffF45920)],
-                ),
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(100.0),
-                ),
-              ),
+            SizedBox(
+              height: 80,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Align(
-                    alignment: Alignment.center,
-                    child:Icon(
-                    Icons.power_input,
-                    size: 100,
+                  Icon(
+                    Icons.blur_on,
                     color: Colors.white,
-                  ),
-                  ),
-                  Align(
-                    
+                    size: 100,
                   ),
                 ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(210),
+                    bottomLeft: Radius.circular(210),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color(0Xffa3aaaf),
+                      offset: Offset(5.0, 10),
+                      blurRadius: 40.0,
+                    )
+                  ],
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(70),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.all(20),
+                        color: Colors.indigo,
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           ],
